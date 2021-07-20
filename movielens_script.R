af883debe72be33bc0554ef43c445f2f5a68c6a8
Iván de Luna <32318756+ivandeluna@users.noreply.github.com>
2021-07-20 17:49:42 -0500

    Update movielens_script.R

--- movielens_script.R
+++ movielens_script.R
@@ -1,10 +1,12 @@
 ### Capstone Movie Lens
 
 
-### EDX Code ##
-##########################################################
+### EDX Code ###
+################################################################################
+#
 # Create edx set, validation set (final hold-out test set)
-##########################################################
+#
+################################################################################
 
 # Note: this process could take a couple of minutes
 
@@ -56,17 +58,21 @@ edx <- rbind(edx, removed)
 
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
-### EDX Code ###
-
 # RMSE function
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings-predicted_ratings)^2,na.rm=T))
 }
 
-# Data exploratory analysis
+
+################################################################################
+#
+## Data exploration
+#
+################################################################################
+
 # Number of rows
 nrow(edx)
-# Number of columns
+# Number of columns or variables
 ncol(edx)
 # Variables and their class
 str(edx)
@@ -74,33 +80,39 @@ str(edx)
 summary(edx) 
 
 # The variable timestamp is of class integer, but it needs to be
-# parsed into datetime format
-class(edx$timestamp)
+# parsed into datetime format, se we need the lubridate library
+# that will help to do it in an easier way.
+
+class(edx$timestamp) # This is in integer format
 edx$timestamp <- as_datetime(edx$timestamp)
-# Also the validatoin set
+
+# Also the validation set
 validation$timestamp <- as_datetime(validation$timestamp)
 
-# Check formatted variable
+# Check formatted variables is in new POSIXt format
 class(edx$timestamp)
 class(validation$timestamp)
 
 
 # Initially the variables userId, movieId, timestamp may not be meaningful
 # but will be used as identification and aggregation variables
-# in order to expand the analysis and have a control of the datasets
+# in order to expand the analysis and have a better organization of the datasets.
 
 # The title variable has a format of "title (year)",
 # so we need to create two columns, for title and for year
 # and apply this to both edx and validation sets
 
+# We can use RegEx or, given that the year component of the title variable is
+# in the last part of the string, as (xxxx), we can subtract it without the parenthesis
+
 edx <- edx %>% mutate(year = as.numeric(str_sub(title, -5,-2)))
 validation <- validation %>% mutate(year = as.numeric(str_sub(title, -5,-2)))
 
-# We also need to substract the title name from title
+# We also need to subtract the title name from title and create the title_name variable
 edx <- edx %>% mutate(title_name = as.character(str_sub(title, end = -8)))
 validation <- validation %>% mutate(title_name = as.character(str_sub(title, end = -8)))
 
-# The rating date can be separated to for better understanding
+# The rating date can also be separated for better understanding
 edx$rating_year <- format(edx$timestamp, "%Y")
 edx$rating_month <- format(edx$timestamp, "%m")
 
@@ -109,14 +121,17 @@ validation$rating_month <- format(validation$timestamp, "%m")
 
 # Genres are also grouped by different names in the same movie title
 # a movie can have multiple genre classifications, so we need to separate them
+# Also, trying to manipulate data with such large strings and classifications
+# can become a problem with memory management
 
 edx <- edx %>% separate_rows(genres, sep="\\|")
 validation <- validation %>% separate_rows(genres, sep="\\|")
 
-######
-# Analysis for rating, title, timestamp and genres
-# Using only edx set
-######
+################################################################################
+#
+## Data Analysis
+#
+################################################################################
 
 #Users
 edx %>% select(userId) %>% 
@@ -126,7 +141,7 @@ edx %>% select(userId) %>%
 # Titles
 edx %>% select(title) %>% 
   n_distinct()
-# there are 10676 unique titles
+# there are 10,676 unique titles
 
 # Ratings
 edx %>% select(rating) %>% 
@@ -143,7 +158,8 @@ edx %>% select(rating) %>%
   group_by(rating) %>% 
   summarise(n = n()) %>% 
   arrange(-n) %>% 
-  ggplot(aes(x = rating, y = n)) + geom_bar(stat = "identity") +
+  ggplot(aes(x = rating, y = n)) + geom_bar(stat = "identity",
+                                            aes(fill = rating)) +
   labs(x = "Rating",
        y = "Rating count",
        title = "Ratings")
@@ -154,9 +170,36 @@ edx %>% select(rating) %>%
 edx %>% select(rating, rating_year) %>% 
   group_by(rating_year) %>% 
   summarise(mean_rating = mean(rating)) %>% 
-  arrange(rating_year) %>% 
-  ggplot(aes(x = rating_year, y = mean_rating)) +
-  geom_bar(stat = "identity")
+  arrange(rating_year)
+
+# There seems to by a correlation between rating year and average ratings
+# which started pretty high, 4.33, but it has normalized between
+# 3.4 and 3.6 band.
+# We need to cast the variable rating_year into numeric for the smoothing process.
+
+edx %>% select(rating, rating_year) %>% 
+  group_by(rating_year) %>% 
+  summarise(mean_rating = mean(rating)) %>% 
+  ggplot(aes(x = as.numeric(rating_year), y = mean_rating)) +
+  geom_point() +
+  geom_smooth()
+
+# If we do the same with movie ratings, there seems to be a preference for
+# classic movies, around 1931-1962
+edx %>% select(rating, year) %>% 
+  group_by(year) %>% 
+  summarise(mean_rating = mean(rating)) %>% 
+  arrange(-mean_rating)
+
+# And movies in general get lower ratings over time or year of release
+
+edx %>% select(rating, year) %>% 
+  group_by(year) %>% 
+  summarise(mean_rating = mean(rating)) %>% 
+  ggplot(aes(x = as.numeric(year), y = mean_rating)) +
+  geom_point() +
+  geom_smooth()
+
 
 # The average rating doesn't change over time, but it can be an effect of
 # 4 being the most common rating.
@@ -168,7 +211,7 @@ edx %>% select(title_name, rating) %>%
             ratings = n()) %>% 
   arrange(-ratings)
 
-# If we concentrate in the top 5 movies, there is a 
+# If we concentrate in the top 5 rated movies, there is a 
 # downward trend in time for a decrease in average ratings until  around 2005,
 # then a considerable increase in the avg rating for the next years.
 # Only Toy Story and True Lies have a descending rating in 2009.
@@ -185,6 +228,16 @@ edx %>% select(title_name, rating_year, rating) %>%
   ggplot(aes(x = rating_year, y = mean_rating, color = title_name,
              group = title_name)) +
   geom_line()
+
+# We can also check that there is a correlation between number of ratings
+# and mean rating, which shows a trend into the values between 3.5 and 4,
+# the more ratings the movie has.
+edx %>% select(title_name, rating) %>% 
+  group_by(title_name) %>% 
+  summarise(mean_rating = mean(rating), ratings = n()) %>% 
+  ggplot(aes(x = ratings, y = mean_rating)) +
+  geom_point() +
+  geom_smooth()
 
 
 # Movies by year
@@ -223,31 +276,32 @@ edx %>% select(title_name) %>%
   mutate(freq = n/sum(n)) %>% 
   arrange(-n)
 
-# Forrest Gump has the most reviews with 124,316, followed by
+# Forrest Gump has the most reviews with 124,316 followed by
 # Toy Story and Jurassic Park
 
 # In the case of genres there is a considerable preference for some
 # such as Drama, Comedy and Action
 edx %>% group_by(genres) %>%
-  summarize(count = n()) %>%
-  arrange(desc(count)) %>%
-  ggplot(aes(genres,count)) + 
-  geom_bar(aes(fill =genres),stat = "identity")+ 
-  labs(title = "Ratings by Genre")+
-  theme(axis.text.x  = element_text(angle= 45, vjust = 50 ))+
-  theme_light()
+  summarize(n = n()) %>%
+  arrange(desc(n)) %>%
+  ggplot(aes(x = genres, y = n)) + 
+  geom_bar(stat = "identity")+ 
+  labs(x = "Genre", y = "Reviews")
 
-# Number of ratings for each user (cambiar)
+# In the case of users, the number of ratings is around 100
 edx %>% count(userId) %>% 
   ggplot(aes(n)) + 
   geom_histogram(bins = 30, binwidth=0.2, color="black", show.legend = FALSE) + 
   scale_x_log10() + 
-  ggtitle("Reviews by User")
+  labs(x = "Number of ratings", y = "Number of users")
 
+################################################################################
+#
+## Data Modeling
+#
+################################################################################
 
-# Data Analysis: Model
-
-# Test set
+# First we create a test set to train the models, according to the specifications
 set.seed(1)
 test_index <- createDataPartition(y = edx$rating, 
                                   times = 1,
@@ -255,90 +309,83 @@ test_index <- createDataPartition(y = edx$rating,
                                   list = FALSE)
 train_set <- edx[-test_index,]
 test_set <- edx[test_index,]
-#to make sure we don???t include users and movies in the test set that do not appear in 
-#the training set, we remove these entries using the semi_join function:
+
+# Remove probable coincidences from the other sets so it doesn't repeat values
+
 test_set <- test_set %>% 
   semi_join(train_set, by = "movieId") %>%
   semi_join(train_set, by = "userId")
 
-# The average rating is
+# First Model, Average or Naive Average
+# The average movie rating is 3.5269
 mu <- mean(train_set$rating)
 mu
 
-
-# The RMSE for mu is
+# The RMSE for mu is not really good, this can be affected by a lot of variables
+# and considerations, such as the trend in time or preference for newer or older movies
 rmse_mu <- RMSE(mu, test_set$rating)
 rmse_mu
 
-rmse_results <- tibble(method = "Average", RMSE = mu_rmse)
+# Create a table to store the results
+rmse_results <- tibble(method = "Average", RMSE = rmse_mu)
 
-# Least Squares
+# Least Squares tries to create a model taking into account
+# some other variables, in this case we will predict our value Y^_i
+# with some values b_i, which in this case will be defined as the
+# mean difference between the actual rating in the training set vs 
+# the mean value (ls_mu)
 ls_mu <- mean(train_set$rating)
 movie_mu <- train_set %>% group_by(movieId) %>% 
   summarize(b_i = mean(rating - ls_mu))
+
+# Which gives us the following values
 movie_mu
 
+# Visualizing it in an histogram, most of the values are near 0, with a
+# skewness towards negative values.
 movie_mu %>% qplot(b_i,
                    geom= "histogram", 
-                   bins = 10, 
-                   data = .,
-                   color = I("black"))
+                   bins = 10,
+                   data = .)
 
+# Now we try to predict ratings given our model LS
 predicted_ratings <- ls_mu + test_set %>% 
   left_join(movie_mu, by="movieId") %>% 
   pull(b_i)
 
+# And we calculate the RMSE to check if it is better that the simple average
 rmse_ls <- RMSE(predicted_ratings , test_set$rating)
+# With a value of 0.9409 it is a little bit better that a simple average
 rmse_ls
 
-rmse_results <- bind_rows(rmse_results, tibble(method = "Least Squares",
+# We store the results in the rmse_results tibble
+rmse_results <- bind_rows(rmse_results, tibble(method = "Least Squares Movie",
                                                RMSE = rmse_ls))
 
-# Third model 
-# let;s compure the user I for , for those who ratedover 100 movies 
-train_set %>% 
+# For the third model we will use user effects
+user_avg <- train_set %>%
   group_by(userId) %>% 
-  summarize(b_u = mean(rating)) %>% 
-  filter(n()>=100) %>%
-  ggplot(aes(b_u)) + 
-  geom_histogram(bins = 30, color = "black")
-#hat there is substantial variability across users ratings as well. T
-#his implies that a further improvement to our 
-#model may be:
-#$Y~u,i~ = ?? + b~i~ + ??~u,i~$
-#we could fit this model by using use the lm() function but as mentioned earlier it 
-#would be very slow
-#lm(rating ~ as.factor(movieId) + as.factor(userId))
-#so here is the code 
-user_avgs <- train_set %>% 
-  left_join(movie_mu, by='movieId') %>%
-  group_by(userId) %>%
-  summarize(b_u = mean(rating - ls_mu - b_i))
-user_avgs
+  left_join(movie_mu, by = "movieId") %>%
+  summarise(b_u = mean(rating - movie_mu - b_i))
 
-#now let's see how RMSE improved this time 
-predicted_ratings <- test_set %>% 
-  left_join(movie_mu, by='movieId') %>%
-  left_join(user_avgs, by='userId') %>%
-  mutate(pred = ls_mu + b_i + b_u) %>%
-  .$pred
+predicted_ratings <- predicted_ratings + test_set %>% 
+  left_join(user_avg, by = "userId") %>% 
+  pull(b_u)
 
-model_3_rmse <- RMSE(predicted_ratings, test_set$rating)
-rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Movie + User Effects Model",  
-                                     RMSE = model_3_rmse))
-rmse_results
+RMSE(predicted_ratings , test_set$rating)
+
 
 ## RMSE of the validation set
 
 valid_pred_rating <- validation %>%
   left_join(movie_mu, by = "movieId" ) %>% 
-  left_join(user_avgs , by = "userId") %>%
+  left_join(user_avg , by = "userId") %>%
   mutate(pred = ls_mu + b_i + b_u ) %>%
   pull(pred)
 
-model_3_valid <- RMSE(validation$rating, valid_pred_rating)
-model_3_valid
+model_valid <- RMSE(validation$rating, valid_pred_rating)
+model_valid
+
 rmse_results <- bind_rows(rmse_results, 
                            tibble(Method = "Validation Results" , RMSE = model_3_valid))
 rmse_results
